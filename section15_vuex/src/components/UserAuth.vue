f77e030f7b7941bce62365431b060beea512cdb4
이한슬 <ihanseul@ihanseuBookPro2.lan>
2020-12-09 11:10:36 +0900

    make store.js

---
+++ section15_vuex/src/components/UserAuth.vue
@@ -0,0 +1,28 @@
+<template>
+  <button @click="login" v-if="!isAuth">Login</button>
+  <button @click="logout" v-else>Logout</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  methods: {
+    //login() {
+    //  this.$store.dispatch('login');
+    //},
+    //logout() {
+    //  this.$store.dispatch('logout');
+    //}
+    ...mapActions({
+      login: 'login',
+      logout: 'logout'
+    })
+  },
+  computed: {
+    isAuth() {
+      return this.$store.getters.userIsAuthenticated;
+    }
+  }
+};
+</script>
